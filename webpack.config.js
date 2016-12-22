64bc399e6f29ad405a37e74f2b33059b73fd387b
Cole Bemis <colebemis@gmail.com>
2016-12-22 00:12:10 -0800

    Remove unnecessary reference to stylus-loader in config

--- webpack.config.js
+++ webpack.config.js
@@ -23,10 +23,6 @@ module.exports = {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/
-      },
-      {
-        test: /\.styl$/,
-        loader: 'stylus-loader'
       }
     ]
   },
