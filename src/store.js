ab1cb9fbe8656ae92583c9a0abfdf3c7859b41a7
Cole Bemis <colebemis@gmail.com>
2016-12-26 19:57:59 -0800

    Move focused tabGroup to top of list

--- src/store.js
+++ src/store.js
@@ -11,8 +11,20 @@ const state = {
 };
 
 const getters = {
-  filteredTabGroups({tabGroups, filterText}) {
-    return tabGroups.map(tabGroup => {
+  orderedTabGroups({tabGroups}) {
+    // make copy of tabGroups array
+    const orderedTabGroups = tabGroups.slice();
+    // find index of focused tabGroup
+    const focusedIndex = tabGroups.findIndex(tabGroup => tabGroup.focused);
+    // remove focused tabGroup from orderedTabGroups and save
+    const focusedTabGroup = orderedTabGroups.splice(focusedIndex, 1)[0];
+    // add focused tabGroup to front of orderedTabGroups
+    orderedTabGroups.unshift(focusedTabGroup);
+
+    return orderedTabGroups;
+  },
+  filteredTabGroups({filterText}, {orderedTabGroups}) {
+    return orderedTabGroups.map(tabGroup => {
       const filteredTabs = {
         tabs: fuseFilter({
           list: tabGroup.tabs,
